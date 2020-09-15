222a04b6a3c8d64664784bc9f7ce01874784dad2
Kim Nguyen <nnhk23@gmail.com>
2020-09-15 01:08:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
       smallest = price
       min_value_item = name
       elsif name_hash.length === 0 
-      min_value_item = nil
+      return NIL
     end
   end
   min_value_item
