03eea57e8fbbded2734b80dd9cec33c34080421d
Kim Nguyen <nnhk23@gmail.com>
2020-09-15 01:00:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  smallest = name_hash[0]["key"]
   name_hash.each do |name, price|
-    
+    if 
   end
 end
