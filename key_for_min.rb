fcc3381b85dd4ab61e4513ead0e3f87d370444e6
Kim Nguyen <nnhk23@gmail.com>
2020-09-15 01:09:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, price|
     if price < smallest
       smallest = price
-      puts min_value_item = name
+       min_value_item = name
       elsif name_hash.length === 0 
-      puts NIL
+       NIL
     end
   end
 end
